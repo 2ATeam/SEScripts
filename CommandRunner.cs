97af89d1ef859be6b3c56025d8a8c7b915515d20
Adya <adya.glu@gmail.com>
2016-03-12 19:59:24 +0200

    Refactored code to support compatibility with Assembler.

--- CommandRunner.cs
+++ CommandRunner.cs
@@ -1,5 +1,4 @@
 ﻿using Sandbox.ModAPI.Ingame;
-using SE_Mods.CommandRunner;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,25 +9,17 @@ namespace SE_Mods.CommandRunner
 {
     // This class extends MyGridProgram which represents actual in-game script. 
     // This encapsulates environment available to the script.
-    
-        
-    /// <summary>
-    /// This script parses and runs available commands with agruments. 
-    /// </summary>
+
     class CommandRunnerScript : MyGridProgram
     {
-        protected void Main(string args)
+        /// <summary>
+        /// This script parses and runs available commands with agruments. 
+        /// </summary>
+        public void Main(string args)
         {
             var c = CommandBuilder.BuildCommand(args);
             c.Run(this);
-            
-        }
 
-        ///// Test stuff here
-        //public static void Main(string[] agrs)
-        //{
-        //    new CommandRunnerScript().Main("AA_Rotate [AA_Name : ROTOR 1 ; AA_Angle : 10 ]");
-        //    Console.Read();
-        //}
+        }
     }
 }
